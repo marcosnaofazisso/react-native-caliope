e9c5c8b1f4b6f17b6d5d3c4e6a02d59915dfb25a
Marcos Vinicius Ferreira <marcosferreiraf22@gmail.com>
2022-09-09 19:36:01 -0300

    clean code

--- MenuBar.js
+++ MenuBar.js
@@ -1,15 +1,10 @@
 import React from "react";
 import { StyleSheet, View, Image } from "react-native";
-import { Avatar, Icon } from "react-native-elements";
-
-
 
 export default function MenuBar() {
     return (
         <View style={styles.container}>
-            {/* <View> */}
                 <Image style={styles.menuBar} source={require('./assets/menu.jpg')} />
-            {/* </View> */}
                 <Image style={styles.avatar} source={require('./assets/avatar-menu.png')} />
         </View>
     );
