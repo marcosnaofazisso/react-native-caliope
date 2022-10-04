69678277b54260814e13af7264ed2c08fe9674ac
Marcos Vinicius Ferreira <marcosferreiraf22@gmail.com>
2022-10-04 20:06:06 -0300

    melhor extensao de fotos

--- data/fotosUsuarios.js
+++ data/fotosUsuarios.js
@@ -1,16 +1,16 @@
 
 const avatarCris = require('../assets/cristine@caliope.com.br.png')
-const avatarPri = require('../assets/priscila@caliope.com.br.jpeg')
-const avatarMaciel = require('../assets/marcosmaciel@caliope.com.br.jpg')
-const avatarJojo = require('../assets/jonathan@caliope.com.br.jpg')
+const avatarPri = require('../assets/priscila@caliope.com.br.png')
+const avatarMaciel = require('../assets/marcosmaciel@caliope.com.br.png')
+const avatarJojo = require('../assets/jonathan@caliope.com.br.png')
 
-const avatarBruno = require('../assets/bruno@caliope.com.br.jpg')
-const avatarDeborah = require('../assets/deborah@caliope.com.br.jpg')
-const avatarMarcelo = require('../assets/marcelo@caliope.com.br.jpg')
+const avatarBruno = require('../assets/bruno@caliope.com.br.png')
+const avatarDeborah = require('../assets/deborah@caliope.com.br.png')
+const avatarMarcelo = require('../assets/marcelo@caliope.com.br.png')
 
-const avatarArnaldo = require('../assets/arnaldo@caliope.com.br.jpg')
-const avatarHenrique = require('../assets/henrique@caliope.com.br.jpg')
-const avatarThiago = require('../assets/thiago@caliope.com.br.jpg')
+const avatarArnaldo = require('../assets/arnaldo@caliope.com.br.png')
+const avatarHenrique = require('../assets/henrique@caliope.com.br.png')
+const avatarThiago = require('../assets/thiago@caliope.com.br.png')
 
 const avatarPadrao = require('../assets/avatar-menu.png')
 
@@ -28,6 +28,6 @@ export const fotosDosUsuariosTeste = {
     'arnaldo@caliope.com.br': avatarArnaldo,
     'henrique@caliope.com.br': avatarHenrique,
     'thiago@caliope.com.br': avatarThiago,
-    
+
     'avatarPadrao': avatarPadrao
 }
